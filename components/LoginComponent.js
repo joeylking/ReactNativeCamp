a0670e26a93c53fa3c9e25df32f68417bb6bcd1a
Joey <30815220+joeylking@users.noreply.github.com>
2021-05-01 14:12:41 -0500

    Week 4 Workshop Assignment

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -4,6 +4,7 @@ import { Input, CheckBox, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import * as ImageManipulator from 'expo-image-manipulator';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -155,10 +156,32 @@ class RegisterTab extends Component {
             });
             if (!capturedImage.cancelled) {
                 console.log(capturedImage);
-                this.setState({imageUrl: capturedImage.uri})
+                this.processImage(capturedImage.uri);
             }
         }
-    }
+    };                                        
+
+    getImageFromGallery = async () => {
+        const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+        if (cameraRollPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (!capturedImage.cancelled) {
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+
+    };
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(imgUri, [{ resize: { width: 400 } }], {format: ImageManipulator.SaveFormat.PNG});
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
+    };
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
@@ -170,7 +193,7 @@ class RegisterTab extends Component {
             SecureStore.deleteItemAsync('userinfo')
                 .catch(error => console.log('Could not delete user info', error));
         }
-    }
+    };
 
     render() {
         return (
@@ -185,6 +208,10 @@ class RegisterTab extends Component {
                         <Button 
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button 
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input 
